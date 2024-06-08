9b1e2077f3ee4e21c454f3825aa3c68b174856bc
Reales Myles <reales.myles@finanzauto.com.co>
2024-06-07 23:28:38 -0500

    Expense detail screen

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,41 +1,120 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Apps
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import data.ExpenseManager
-import data.ExpenseRepoImpl
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import data.TitleTopBarTypes
 import moe.tlaster.precompose.PreComposeApp
-import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
-import moe.tlaster.precompose.viewmodel.viewModel
-import presentacion.ExpensesUiState
-import presentacion.ExpensesViewModel
-import ui.ExpensesScreen
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.path
+import moe.tlaster.precompose.navigation.rememberNavigator
+import navigation.Navigation
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     PreComposeApp {
         val colors = getColorsTheme()
-        val viewModel= viewModel(modelClass = ExpensesViewModel::class){
-            ExpensesViewModel(ExpenseRepoImpl(ExpenseManager))
-        }
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
         AppTheme {
-            ExpensesScreen(
-                uiState = uiState, onExpenseClick = {})
+            val navigator = rememberNavigator()
+            val titleTopBar = getTitleTopAppBar(navigator)
+            val isEditOrAddExpenses = titleTopBar != TitleTopBarTypes.DASHBOARD.value
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(elevation = 0.dp,
+                            backgroundColor = colors.backgroundColor,
+                            title = {
+                                Text(
+                                    text = titleTopBar,
+                                    fontSize = 25.sp,
+                                    color = colors.textColor
+                                )
+                            },
+                            navigationIcon = {
+                                if (isEditOrAddExpenses){
+                                    IconButton(
+                                        onClick = {
+                                            navigator.popBackStack()
+                                        }
+                                    ) {
+                                        Icon(
+                                            modifier = Modifier.padding(start = 16.dp),
+                                            imageVector = Icons.Default.ArrowBackIosNew,
+                                            tint = colors.textColor,
+                                            contentDescription = "Back arrow icon"
+                                        )
+                                    }
+                                }else{
+                                    Icon(
+                                        modifier = Modifier.padding(start = 16.dp),
+                                        imageVector = Icons.Default.Apps,
+                                        tint = colors.textColor,
+                                        contentDescription = "Dashboard icon"
+                                    )
+                                }
+
+                            }
+
+                        )
+
+                    },
+                    floatingActionButton = {
+                        if (!isEditOrAddExpenses){
+                            FloatingActionButton(
+                                modifier = Modifier.padding(8.dp),
+                                onClick = {
+                                    navigator.navigate("/addExpenses")
+                                },
+                                shape = RoundedCornerShape(50),
+                                backgroundColor = colors.addIconColor,
+                                contentColor = Color.White
+                            ){
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    tint = Color.White,
+                                    contentDescription = "Floating icon"
+                                )
+                            }
+                        }
+                    }
+                ) {
+                    Navigation(navigator)
+                }
         }
     }
+}
 
+@Composable
+fun getTitleTopAppBar(navigator: Navigator): String {
+
+    var titleTopBar = TitleTopBarTypes.DASHBOARD
+
+    val isOnAddExpenses =
+        navigator.currentEntry.collectAsState(null).value?.route?.route.equals("/addExpenses/{id}")
+    if (isOnAddExpenses){
+        titleTopBar = TitleTopBarTypes.ADD
+    }
+
+    val isOnEditExpense = navigator.currentEntry.collectAsState(null).value?.path<Long>("id")
+    isOnEditExpense?.let {
+        titleTopBar = TitleTopBarTypes.EDIT
+    }
+    return titleTopBar.value
 }
---
+++ composeApp/src/commonMain/kotlin/ui/ExpensesDetailScreen.kt
@@ -0,0 +1,9 @@
+package ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ExpensesDetailScreen(){
+
+}
