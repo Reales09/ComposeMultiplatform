3592073304cdaa9b26c4ac0e6b5812afbf56536a
Reales Myles <rclarence123@gmail.com>
2024-06-10 01:18:33 -0500

    Given when then - Primeros test 2/2

--- composeApp/src/commonTest/kotlin/ExampleTest.kt
+++ composeApp/src/commonTest/kotlin/ExampleTest.kt
@@ -1,4 +1,7 @@
+import model.Expense
+import model.ExpenseCategory
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
@@ -25,5 +28,50 @@ class ExampleTest {
 
         // Then
         assertNotEquals(10, result)
+    }
+
+    @Test
+
+    fun expense_model_test(){
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.5, category = ExpenseCategory.CAR, description = "Combustible")
+
+        // When
+        expenseList.add(expense)
+
+        // Then
+        assertContains(expenseList, expense)
+    }
+
+    @Test
+    fun expense_model_param_test_success(){
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.5, category = ExpenseCategory.OTHER, description = "Combustible")
+        val expense2 = Expense(id = 2, amount = 8.9, category = ExpenseCategory.OTHER, description = "Limpieza")
+
+
+        // When
+        expenseList.add(expense)
+        expenseList.add(expense2)
+
+        // Then
+        assertEquals(expenseList[0].category, expenseList[1].category)
+    }
+    @Test
+    fun expense_model_param_test_fail(){
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.5, category = ExpenseCategory.CAR, description = "Combustible")
+        val expense2 = Expense(id = 2, amount = 8.9, category = ExpenseCategory.OTHER, description = "Limpieza")
+
+
+        // When
+        expenseList.add(expense)
+        expenseList.add(expense2)
+
+        // Then
+        assertNotEquals(expenseList[0].category, expenseList[1].category)
     }
 }
